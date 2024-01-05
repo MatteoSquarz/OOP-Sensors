ec164441e0d86919ea91ff597eac8e02cfcdde11
MatteoSquarz <93669086+MatteoSquarz@users.noreply.github.com>
2024-01-05 15:31:35 +0100

    qtoolbar added

--- View/MainWindow.cpp
+++ View/MainWindow.cpp
@@ -6,16 +6,29 @@
 #include <QApplication>
 #include <QFile>
 #include <QFileDialog>
+#include <QToolBar>
 #include <QTextStream>
 #include <iostream>
 namespace Sensor{
 namespace View{
 
 MainWindow::MainWindow(std::vector<AbstractSensor*>& sensorList) : sensorList(sensorList){
-
-    auto* openAction = this->menuBar()->addAction("OPEN");
-    connect(openAction, &QAction::triggered, [this](bool checked){open();});
-    auto* saveAction = this->menuBar()->addAction("SAVE");
+    this->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+    QToolBar* toolBar = new QToolBar();
+    QAction* open = new QAction(QIcon(QPixmap(("assets/folder.png"))), "Apri");
+    toolBar->addAction(open);
+    QAction* save = new QAction(QIcon(QPixmap(("assets/diskette.png"))), "Salva");
+    toolBar->addSeparator();
+    toolBar->addAction(save);
+    toolBar->setFixedHeight(30);
+    toolBar->setMovable(false);
+    this->addToolBar(toolBar);
+    //open->setText("Apri");
+    //this->menuBar()->addAction(open);
+    //connect(open, &QAction::triggered, [this](bool checked){open();});
+    connect(open, &QAction::triggered, this, &MainWindow::open);
+    //QAction* save = new QAction(QIcon("assets/diskette.png"), "Salva", this);
+    //this->menuBar()->addAction(save);
     ApplicationPanel* sensore = new ApplicationPanel(sensorList);
     setCentralWidget(sensore);
 
